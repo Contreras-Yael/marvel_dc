8c70c3753ee717796927d8a515b8071d9df4987c
Contreras-Yael <yaelrios243@gmail.com>
2026-06-30 13:22:00 -0700

    search en nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
-
+import { SearchBarComponent } from '../search-bar-component/search-bar-component.component';
 
 @Component({
   selector: 'navbar-sup',
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink,RouterOutlet,SearchBarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
