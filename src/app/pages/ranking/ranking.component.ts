2293675e64b011692ae1fb2085862af3b9e83cc9
Contreras-Yael <yaelrios243@gmail.com>
2026-06-30 13:14:52 -0700

    finale

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ApiheroService } from '../../services/apihero.service';
 
 @Component({
@@ -7,12 +7,24 @@ import { ApiheroService } from '../../services/apihero.service';
   templateUrl: './ranking.component.html',
   styleUrl: './ranking.component.css'
 })
-export class RankingComponent {
+export class RankingComponent implements OnInit {
 
   private api = inject(ApiheroService);
-
+  public heroservice : any[] =[];
   public top: any[] =[];
 
+  ngOnInit(): void {
+
+  }
+
+  top_heroe(){
+    this.api.sups_list().subscribe({
+      next:(data)=>{
+      this.heroservice = data;
+      console.log('Cargado o eso parece', this.heroservice);
+      },
+    })
+  }
 
 
 }
